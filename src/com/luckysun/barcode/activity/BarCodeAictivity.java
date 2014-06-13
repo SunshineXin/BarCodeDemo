7d5b3b7bc67a8d2e76fd73a2e8b3fd1a1031f668
SunshineXin <303447222@qq.com>
2014-06-13 15:22:22 +0800

    cremt BarCode project.

---
+++ src/com/luckysun/barcode/activity/BarCodeAictivity.java
@@ -0,0 +1,18 @@
+package com.luckysun.barcode.activity;
+
+import com.luckysun.barcode.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BarCodeAictivity extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+	}
+
+}
